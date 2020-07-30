b1d7f58452854d192430a490c16707fec95253f2
HilayMehta <hilay.mehta@learner.manipal.edu>
2020-07-30 11:23:02 +0530

    Rugved ROS Task 1

---
+++ rugved/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/rugved/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/rugved/devel;/home/ubuntu/mybot_ws/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/rugved/devel;/home/ubuntu/mybot_ws/devel;/opt/ros/melodic' != '' else []
